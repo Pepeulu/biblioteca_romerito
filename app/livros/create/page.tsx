bfb8eb619168ebb2bfdd05901aa69c540c029d13
dargao <pepeulu2007@gmail.com>
2026-01-28 17:12:50 -0300

    forms

---
+++ app/livros/create/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import CreateLivroComponent from "../../../components/CreateLivroComponent";
+import "./create.css";
+
+export default function Home() {
+  return (
+    <div className="conteudo">
+      <CreateLivroComponent>forms 1</CreateLivroComponent>
+
+    </div>
+  );
+}
--- components/CreateLivroComponent.tsx
+++ components/CreateLivroComponent.tsx
@@ -1,18 +1,57 @@
 'use client'
 
 import React from "react"
+import axios from "axios"
 
 type CreateLivroComponentProps = {
-  children: React.ReactNode
+  children?: React.ReactNode
 }
 
-export default function CreateLivroComponent({ children, }: CreateLivroComponentProps) {
+export default function CreateLivroComponent({ children }: CreateLivroComponentProps) {
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    const formData = new FormData(e.currentTarget)
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/livros/",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      )
+
+      console.log("Livro criado:", response.data)
+
+    } catch (error) {
+      console.error("Erro ao criar livro:", error)
+    }
+  }
+
   return (
-    <div className='form-container'>
-        <form method="post">
-            <input type="text" />
-        </form>
-      {children}
+    <div className="form-container">
+      <form onSubmit={handleSubmit} encType="multipart/form-data">
+
+        <input type="text" name="nome_livro" placeholder="Nome do livro" required />
+
+        <input type="date" name="Ano_de_publicacao" required />
+
+        <input type="text" name="ISBN" required placeholder="ISBN"/>
+
+        <input type="file" name="foto_livro" accept="image/*" />
+
+        <input type="number" name="autor" value={1} readOnly />
+
+        <input type="file" name="livro_arquivo" accept=".pdf" />
+
+        <button type="submit">Salvar</button>
+
+        {children}
+      </form>
     </div>
   )
 }
